09fe813be81940f4d53cf93b6b251ba246788ac8
Srikar KSV <srikar1awesome@gmail.com>
2020-10-24 16:52:47 +0530

    📦 NEW: Prototyped the JS to append iframes from the links.

---
+++ frontend/app.js
@@ -0,0 +1,19 @@
+const videoBtn = document.querySelector(".get-video-btn");
+const videoSection = document.querySelector(".videos");
+
+videoBtn.addEventListener("click", handleVideoBtn);
+
+function handleVideoBtn() {
+  const videoLinks = document.querySelector("#link-field").value.split(",");
+  videoLinks.forEach((videoLink) => {
+    videoLink = videoLink.trim();
+    const videoId = videoLink.split("=")[1].split("&")[0];
+    const iframeHtml = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" 
+                        frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+                        encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    const iframHtmlFragment = document
+      .createRange()
+      .createContextualFragment(iframeHtml);
+    videoSection.appendChild(iframHtmlFragment);
+  });
+}
