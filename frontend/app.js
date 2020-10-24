c746fe1b1e567381ba2e29e880a355002f7ef7cf
Srikar KSV <srikar1awesome@gmail.com>
2020-10-24 18:34:37 +0530

    👌 IMPROVE: Changed the html to be appended in JS.

--- frontend/app.js
+++ frontend/app.js
@@ -8,9 +8,12 @@ function handleVideoBtn() {
   videoLinks.forEach((videoLink) => {
     videoLink = videoLink.trim();
     const videoId = videoLink.split("=")[1].split("&")[0];
-    const iframeHtml = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" 
+    const iframeHtml = `<div class="videoWrapper">
+                        <iframe width="560" height="315" 
+                        src="https://www.youtube.com/embed/${videoId}" 
                         frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
-                        encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+                        encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>`;
     const iframHtmlFragment = document
       .createRange()
       .createContextualFragment(iframeHtml);
