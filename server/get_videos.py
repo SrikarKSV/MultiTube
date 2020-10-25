73117f67788122458edff7d0b9e88ebf2ed4c582
Srikar KSV <srikar1awesome@gmail.com>
2020-10-25 18:07:21 +0530

    🐛 FIX: Fixed to handle errors.

--- server/get_videos.py
+++ server/get_videos.py
@@ -1,11 +1,8 @@
 import requests
-import os
-from pprint import pprint
-
 
 class GetVideoId:
-    def __init__(self) -> None:
-        self.API_KEY = os.environ.get("MultiTubeYTKEY")
+    def __init__(self, api_key: str) -> None:
+        self.API_KEY = api_key  # os.environ.get("MultiTubeYTKEY")
 
     def get_playlist_videos(self, playlist_id: str, next_page_token: str) -> dict:
         params = {
@@ -17,18 +14,22 @@ class GetVideoId:
         }
 
         r = requests.get(
-            "https://www.googleapis.com/youtube/v3/playlistItems", 
-            params=params
+            "https://www.googleapis.com/youtube/v3/playlistItems", params=params
         )
+
         playlist_data = r.json()
-        video_ids = []
-        for video_id in playlist_data["items"]:
-            video_id = video_id["contentDetails"]["videoId"]
-            video_ids.append("https://www.youtube.com/watch?v=" + video_id)
+        try:
+            video_ids = []
+            for video_id in playlist_data["items"]:
+                video_id = video_id["contentDetails"]["videoId"]
+                video_ids.append("https://www.youtube.com/watch?v=" + video_id)
 
-        next_page_token = playlist_data.get("nextPageToken")
+            next_page_token = playlist_data.get("nextPageToken")
 
-        return {"video_ids": video_ids, "nextPageToken": next_page_token}
+            return {"video_ids": video_ids, "nextPageToken": next_page_token}
+        except KeyError:
+            error_code = playlist_data["error"]["code"]
+            return {"error": error_code}
 
     def get_channel_videos(self, channel_id: str, next_page_token: str) -> dict:
         id = "id" if channel_id[:2] == "UC" else "forUsername"
@@ -36,15 +37,18 @@ class GetVideoId:
             "key": self.API_KEY,
             id: channel_id,
             "part": "contentDetails",
-            "maxResults": "15",
             "pageToken": next_page_token,
         }
 
         r = requests.get(
-            "https://www.googleapis.com/youtube/v3/channels", 
-            params=params
+            "https://www.googleapis.com/youtube/v3/channels", params=params
         )
-        upload_id = r.json()["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
-        videos_ids = self.get_playlist_videos(upload_id, next_page_token)
 
+        channel_data = r.json()
+        try:
+            upload_id = channel_data["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+        except KeyError:
+            return {"error": 403} if channel_data.get("error") else {"error": 404}
+
+        videos_ids = self.get_playlist_videos(upload_id, next_page_token)
         return videos_ids
