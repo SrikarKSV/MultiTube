7c7f31dd4cf67eab72aa83472ba52e1cdb1a7a82
Srikar KSV <srikar1awesome@gmail.com>
2020-10-25 16:48:51 +0530

    👌 IMPROVE: Converted the script into OOP structure.

--- server/get_videos.py
+++ server/get_videos.py
@@ -2,43 +2,49 @@ import requests
 import os
 from pprint import pprint
 
-API_KEY = os.environ.get('MultiTubeYTKEY')
 
+class GetVideoId:
+    def __init__(self) -> None:
+        self.API_KEY = os.environ.get("MultiTubeYTKEY")
 
-def get_playlist_videos(playlist_id: str):
-    params = {
-        "key": API_KEY,
-        "playlistId": playlist_id,
-        "part": "contentDetails",
-        "maxResults": "15"
-    }
+    def get_playlist_videos(self, playlist_id: str, next_page_token: str) -> dict:
+        params = {
+            "key": self.API_KEY,
+            "playlistId": playlist_id,
+            "part": "contentDetails",
+            "maxResults": "15",
+            "pageToken": next_page_token,
+        }
 
-    r = requests.get(
-        'https://www.googleapis.com/youtube/v3/playlistItems', params=params)
-    playlist_data = r.json()
-    video_ids = []
-    for video_id in playlist_data["items"]:
-        video_id = video_id["contentDetails"]["videoId"]
-        video_ids.append("https://www.youtube.com/watch?v=" + video_id)
-    
-    next_page_token = playlist_data.get("nextPageToken")
+        r = requests.get(
+            "https://www.googleapis.com/youtube/v3/playlistItems", 
+            params=params
+        )
+        playlist_data = r.json()
+        video_ids = []
+        for video_id in playlist_data["items"]:
+            video_id = video_id["contentDetails"]["videoId"]
+            video_ids.append("https://www.youtube.com/watch?v=" + video_id)
 
-    return {"video_ids": video_ids, "nextPageToken":next_page_token}
+        next_page_token = playlist_data.get("nextPageToken")
 
+        return {"video_ids": video_ids, "nextPageToken": next_page_token}
 
-def get_channel_videos(channel_id: str):
-    id = "id" if channel_id[:2] == "UC" else "forUsername"
-    params = {
-        "key": API_KEY,
-        id: channel_id,
-        "part": "contentDetails",
-        "maxResults": "15"
-    }
+    def get_channel_videos(self, channel_id: str, next_page_token: str) -> dict:
+        id = "id" if channel_id[:2] == "UC" else "forUsername"
+        params = {
+            "key": self.API_KEY,
+            id: channel_id,
+            "part": "contentDetails",
+            "maxResults": "15",
+            "pageToken": next_page_token,
+        }
 
-    r = requests.get(
-        "https://www.googleapis.com/youtube/v3/channels", params=params)
-    upload_id = r.json()[
-        "items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
-    videos_ids = get_playlist_videos(upload_id)
+        r = requests.get(
+            "https://www.googleapis.com/youtube/v3/channels", 
+            params=params
+        )
+        upload_id = r.json()["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+        videos_ids = self.get_playlist_videos(upload_id, next_page_token)
 
-    return videos_ids
+        return videos_ids
